81d7310b98ecc23daccb751cc5722b65635c9902
maxim-polyakov <maxim7012@gmail.com>
2025-07-02 18:39:00 +0300

    Update index.js

--- api-gateway/src/index.js
+++ api-gateway/src/index.js
@@ -2,76 +2,22 @@ const express = require('express')
 const dotenv = require('dotenv')
 const cors = require('cors')
 const fetch = require('node-fetch');
-
+const httpProxy = require('http-proxy')
+const proxy = httpProxy.createProxyServer();
 dotenv.config();
 
 const app = express();
 app.use(cors());
+
 // proxy configuration
-app.get('/users', async (req, res) => {
-    try {
-        // внешний api, к которому мы обращаемся
-        const externalapiurl = 'http://user-service - 1:3001';
-
-        // получаем данные с внешнего api
-        const response = await fetch(externalapiurl);
-
-        if (!response.ok) {
-            throw new error(`ошибка при запросе: ${response.status}`);
-        }
-
-        const data = await response.json();
-
-        // возвращаем данные клиенту
-        res.json(data);
-    } catch (error) {
-        console.error('ошибка api gateway:', error.message);
-        res.status(500).json({ error });
-    }
+app.use('/users', (req, res) => {
+    proxy.web(req, res, {target: 'http://user-service:3001', changeOrigin: true});
 });
-
-app.get('/products', async (req, res) => {
-    try {
-        // внешний api, к которому мы обращаемся
-        const externalapiurl = 'http://product-service - 1:3003';
-
-        // получаем данные с внешнего api
-        const response = await fetch(externalapiurl);
-
-        if (!response.ok) {
-            throw new error(`ошибка при запросе: ${response.status}`);
-        }
-k
-        const data = await response.json();
-
-        // возвращаем данные клиенту
-        res.json(data);
-    } catch (error) {
-        console.error('ошибка api gateway:', error.message);
-        res.status(500).json({ error });
-    }
+app.use('/products', (req, res) => {
+    proxy.web(req, res, {target: 'http://product-service:3003', changeOrigin: true});
 });
-
-app.get('/orders', async (req, res) => {
-    try {
-        // внешний api, к которому мы обращаемся
-        const externalapiurl = 'http://order-service - 1:3002';
-
-        // получаем данные с внешнего api
-        const response = await fetch(externalapiurl);
-
-        if (!response.ok) {
-            throw new error(`ошибка при запросе: ${response.status}`);
-        }
-
-        const data = await response.json();
-
-        // возвращаем данные клиенту
-        res.json(data);
-    } catch (error) {
-        console.error('ошибка api gateway:', error.message);
-        res.status(500).json({ error });
-    }
+app.use('/orders', (req, res) => {
+    proxy.web(req, res, {target: 'http://order-service:3002', changeOrigin: true});
 });
 
 
