aaaee39510a1b4c068e27e5cd733ff93053f2511
maxim-polyakov <maxim7012@gmail.com>
2025-07-02 18:57:48 +0300

    Update index.js

--- api-gateway/src/index.js
+++ api-gateway/src/index.js
@@ -1,28 +1,17 @@
 const express = require('express')
 const dotenv = require('dotenv')
 const cors = require('cors')
-const { createproxymiddleware } = require('http-proxy-middleware');
+const { createProxyMiddleware } = require('http-proxy-middleware');
 
 dotenv.config();
 
 const app = express();
 app.use(cors());
 
-app.use('/users', createproxymiddleware({
-    target: 'http://user:3001', // адрес user service
-    changeorigin: true
-}));
 
-app.use('/products', createproxymiddleware({
-    target: 'http://product:3003', // адрес product service
-    changeorigin: true
-}));
-
-
-app.use('/orders', createproxymiddleware({
-    target: 'http://order:3002', // адрес order service
-    changeorigin: true
-}));
+app.use('/users', createProxyMiddleware({ target: 'http://user:3001', changeOrigin: true, }))
+app.use('/products', createProxyMiddleware({ target: 'http://product:3003', changeOrigin: true, }))
+app.use('/orders', createProxyMiddleware({ target: 'http://order:3002', changeOrigin: true, }))
 
 const PORT = process.env.PORT || 3000;
 
