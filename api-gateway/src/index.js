b7e81be76b41c0576624f192fdf131341d359fd7
maxim-polyakov <maxim7012@gmail.com>
2025-07-02 03:24:33 +0300

    Update index.js

--- api-gateway/src/index.js
+++ api-gateway/src/index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const dotenv = require('dotenv')
 const cors = require('cors')
-const fetch = require('node-fetch');
+import fetch from "node-fetch";
 
 const proxy = httpProxy.createProxyServer();
 
